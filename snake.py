07a9c4aa4aea9df7243673bd4ff27ae303d6cbe2
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2025-05-17 19:42:13 +0200

    Ajouts
    feat : Nous initialisons Pygame et configurons notre fenêtre d'affichage. pygame.display.set_mode()Nous créons une fenêtre et renvoyons un Surfaceobjet représentant l'écran. Nous créons également un Clockobjet qui contrôlera la fréquence d'images du jeu
    
    feat : Nous initialisons la position du serpent au centre de l'écran. Sa vitesse est fixée à 20 pixels par image dans la direction Y. Nous la définissons également teleport_wallssur True, ce qui lui permettra de traverser les murs et d'apparaître de l'autre côté de l'écran. Si False, traverser un mur entraînera la fin de la partie.

--- snake.py
+++ snake.py
@@ -7,3 +7,21 @@ BLOCK_SIZE = 20
 pygame.font.init()
 FONT = pygame.font.SysFont("Consolas", 20) # Ou toute autre police que vous aimeriez
 score = 0
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+# Initialiser pygame
+pygame.init()
+
+# Configuration de l'affichage
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Mise en place de l'horloge
+clock = pygame.time.Clock()
+
+# Initialisation du serpent et de la nourriture
+snake_pos = [[WIDTH // 2, HEIGHT // 2]]
+snake_speed = [0, BLOCK_SIZE]
+
+teleport_walls = True
