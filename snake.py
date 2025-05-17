b470ab062d07868e3f52af56085615c67f1a87a9
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2025-05-17 19:37:09 +0200

    Ajouts
    feat : Nous définissons ici la largeur et la hauteur de notre fenêtre de jeu, toutes deux fixées à 600 pixels. La largeur BLOCK_SIZEest fixée à 20, ce qui correspond à la taille de nos blocs de serpent et de nourriture.
    
    feat : Nous initialisons le fontmodule dans pygame, qui nous permet d'afficher du texte sur la fenêtre de jeu. Nous configurons également une score_fontvariable qui servira à afficher le score. scoreLa variable est initialisée à 0.

--- snake.py
+++ snake.py
@@ -1,2 +1,9 @@
 import pygame
 import random
+
+WIDTH, HEIGHT = 600, 600
+BLOCK_SIZE = 20
+
+pygame.font.init()
+FONT = pygame.font.SysFont("Consolas", 20) # Ou toute autre police que vous aimeriez
+score = 0
