8d7be81c901fc6a356f22e4c9936dec0d0ee9c40
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2025-05-18 19:04:00 +0200

    Feat / Fix

--- snake.py
+++ snake.py
@@ -2,143 +2,141 @@ import pygame
 import sys
 import random
 
-WIDTH, HEIGHT = 600, 600
-BLOCK_SIZE = 20
+class Block:
+    def __init__(self, x_pos, y_pos):
+        self.x = x_pos
+        self.y = y_pos
 
-pygame.display.set_caption("Snake Game")
-pygame.font.init()
-score_font = pygame.font.SysFont("Consolas", 20)
-score = 0
+class Food:
+    def __init__(self):
+        x = random.randint(0, NB_COL - 1)
+        y = random.randint(0, NB_ROW - 1)
+        self.block = Block(x, y)
 
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
+    def draw_food(self, screen):
+        rect = pygame.Rect(self.block.x * CELL_SIZE, self.block.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+        pygame.draw.rect(screen, (72, 212, 98), rect)
 
-# Initialiser pygame
+class Snake:
+    def __init__(self):
+        self.body = [Block(2, 6), Block(3, 6), Block(4, 6)]
+        self.direction = "RIGHT"
+
+    def draw_snake(self, screen):
+        for block in self.body:
+            x_coord = block.x * CELL_SIZE
+            y_coord = block.y * CELL_SIZE
+            block_rect = pygame.Rect(x_coord, y_coord, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, (83, 177, 253), block_rect)
+
+    def move_snake(self):
+        snake_block_count = len(self.body)
+        old_head = self.body[snake_block_count - 1]
+
+        if self.direction == "RIGHT":
+            new_head = Block(old_head.x + 1, old_head.y)
+        elif self.direction == "LEFT":
+            new_head = Block(old_head.x - 1, old_head.y)
+        elif self.direction == "TOP":
+            new_head = Block(old_head.x, old_head.y - 1)
+        else:
+            new_head = Block(old_head.x, old_head.y + 1)
+
+        self.body.append(new_head)
+        self.body.pop(0)
+
+    def check_collision(self):
+        head = self.body[-1]
+        # Wall collision
+        if (head.x >= NB_COL or head.x < 0 or head.y >= NB_ROW or head.y < 0):
+            return True
+        # Self collision
+        for block in self.body[:-1]:
+            if block.x == head.x and block.y == head.y:
+                return True
+        return False
+
+    def check_food_collision(self, food):
+        head = self.body[-1]
+        if head.x == food.block.x and head.y == food.block.y:
+            self.body.insert(0, Block(-1, -1))  # Add dummy block that will be removed
+            return True
+        return False
+
+class Game:
+    def __init__(self):
+        self.snake = Snake()
+        self.food = Food()
+        self.score = 0
+
+    def update(self):
+        self.snake.move_snake()
+        if self.snake.check_collision():
+            self.game_over()
+        if self.snake.check_food_collision(self.food):
+            self.score += 1
+            self.food = Food()
+
+    def draw(self, screen):
+        self.snake.draw_snake(screen)
+        self.food.draw_food(screen)
+        # Draw score
+        font = pygame.font.SysFont(None, 36)
+        score_text = font.render(f'Score: {self.score}', True, (0, 0, 0))
+        screen.blit(score_text, (10, 10))
+
+    def game_over(self):
+        global game_on
+        game_on = False
+
 pygame.init()
 
 NB_COL = 10
 NB_ROW = 15
 CELL_SIZE = 40
 
+pygame.display.set_caption("Snake Game - Sidiki")
 screen = pygame.display.set_mode(size=(NB_COL * CELL_SIZE, NB_ROW * CELL_SIZE))
 timer = pygame.time.Clock()
 
-# Initialisation du serpent et de la nourriture
-snake_pos = [[WIDTH // 2, HEIGHT // 2]]
-snake_speed = [0, BLOCK_SIZE]
+game_on = True
+game = Game()
 
-teleport_walls = True
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 400)
 
-def generate_food():
-    while True:
-        x = random.randint(0, (WIDTH - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
-        y = random.randint(0, (HEIGHT - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
-        food_pos = [x, y]
-        if food_pos not in snake_pos:
-            return food_pos
+def show_grid():
+    for i in range(0, NB_COL):
+        for j in range(0, NB_ROW):
+            rect = pygame.Rect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, (0, 0, 0), rect, width=1)
 
-def draw_objects():
-    win.fill((0, 0, 0))
-    for pos in snake_pos:
-        pygame.draw.rect(win, WHITE, pygame.Rect(pos[0], pos[1], BLOCK_SIZE, BLOCK_SIZE))
-    pygame.draw.rect(win, RED, pygame.Rect(food_pos[0], food_pos[1], BLOCK_SIZE, BLOCK_SIZE))
-    # Afficher le score
-    score_text = score_font.render(f"Score: {score}", True, WHITE)
-    win.blit(score_text, (10, 10))
+while game_on:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_on = False
+            sys.exit()
+        if event.type == SCREEN_UPDATE:
+            game.update()
 
-def update_snake():
-    global food_pos, score
-    new_head = [snake_pos[0][0] + snake_speed[0], snake_pos[0][1] + snake_speed[1]]
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                if game.snake.direction != "DOWN":
+                    game.snake.direction = "TOP"
+            elif event.key == pygame.K_DOWN:
+                if game.snake.direction != "TOP":
+                    game.snake.direction = "DOWN"
+            elif event.key == pygame.K_LEFT:
+                if game.snake.direction != "RIGHT":
+                    game.snake.direction = "LEFT"
+            elif event.key == pygame.K_RIGHT:
+                if game.snake.direction != "LEFT":
+                    game.snake.direction = "RIGHT"
 
-    if teleport_walls:
-        # si la nouvelle position de la tête est en dehors de l'écran, la ramener de l'autre côté
-        if new_head[0] >= WIDTH:
-            new_head[0] = 0
-        elif new_head[0] < 0:
-            new_head[0] = WIDTH - BLOCK_SIZE
-        if new_head[1] >= HEIGHT:
-            new_head[1] = 0
-        elif new_head[1] < 0:
-            new_head[1] = HEIGHT - BLOCK_SIZE
-    
-    if new_head == food_pos:
-        food_pos = generate_food() # Nouvelle nourriture
-        score += 1 # Incrémenter le score lorsque la nourriture est mangée
-    else:
-        snake_pos.pop() # Enlève la dernière partie du serpent
-    
-    snake_pos.insert(0, new_head) # Ajoute la nouvelle tête à la position du serpent
-
-def game_over():
-    # fin de partie lorsque le serpent heurte les limites ou se heurte lui-même
-    if teleport_walls:
-        return snake_pos[0] in snake_pos[1:]
-    else:
-        return snake_pos[0] in snake_pos[1:] or \
-            snake_pos[0][0] >= WIDTH or \
-            snake_pos[0][0] < 0 or \
-            snake_pos[0][1] >= HEIGHT or \
-            snake_pos[0][1] < 0
-   
-def game_over_screen():
-    global score
-    win.fill((0, 0, 0))
-    game_over_font = pygame.font.SysFont("consolas", 50)
-    game_over_text = game_over_font.render(f"Snake - Game Over ! Score : {score}", True, WHITE)
-    win.blit(game_over_text, (WIDTH // 2 - game_over_text.get_width() // 2, HEIGHT // 2 - game_over_text.get_height() // 2))
+    screen.fill((255, 255, 255))
+    show_grid()
+    game.draw(screen)
     pygame.display.update()
+    timer.tick(60)
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    run() # Redémarre le jeu
-                    return
-                elif event.key == pygame.K_q:
-                    pygame.quit() # Quitte le jeu
-                    return
-            
-def run():
-    global snake_speed, snake_pos, food_pos, score
-    snake_pos = [[WIDTH // 2, HEIGHT // 2]]
-    snake_speed = [0, BLOCK_SIZE]
-    food_pos = generate_food()
-    score = 0
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            # lorsque HAUT est pressé mais que le serpent va vers le bas, ignorer l'entrée
-            if snake_speed[1] != BLOCK_SIZE:
-                snake_speed = [0, -BLOCK_SIZE]
-        elif keys[pygame.K_DOWN]:
-            # lorsque BAS est pressé mais que le serpent va vers le haut, ignorer l'entrée
-            if snake_speed[1] != -BLOCK_SIZE:
-                snake_speed = [0, BLOCK_SIZE]
-        elif keys[pygame.K_LEFT]:
-            # lorsque GAUCHE est pressé mais que le serpent va vers la droite, ignorer l'entrée
-            if snake_speed[0] != BLOCK_SIZE:
-                snake_speed = [-BLOCK_SIZE, 0]
-        elif keys[pygame.K_RIGHT]:
-            # lorsque DROITE est pressé mais que le serpent va vers la gauche, ignorer l'entrée
-            if snake_speed[0] != -BLOCK_SIZE:
-                snake_speed = [BLOCK_SIZE, 0]
-
-        if game_over():
-            game_over_screen()
-            return
-        
-        update_snake()
-        draw_objects()
-        pygame.display.update()
-        clock.tick(15) # Limite la vitesse du serpent à 15 FPS
-
-if __name__ == "__main__":
-    run()
+pygame.quit()
